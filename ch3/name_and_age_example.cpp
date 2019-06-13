f750c953a3e99663d183f1d07d961e23755b7de3
Alan Mond <alan.mond@gmail.com>
2019-06-13 10:12:55 -0700

    changed example to age in months

--- ch3/name_and_age_example.cpp
+++ ch3/name_and_age_example.cpp
@@ -3,8 +3,11 @@ int main()
 {
     cout << "Please enter your first name and age\n"; // prompts first name and age" 
     string first_name = "???"; 
-    int age = 0;
-    cin >> first_name >> age;
-    cout << "Hello, " << first_name << " (age "<< age <<")\n";
+    double age = 0.0; // modified for children who can be very proud of being 5.5 years old
+    int age_in_months = 0; // initialize age in months
+    cin >> first_name >> age; // get name and age from console
+    age_in_months = age * 12 ; // convert age to months
+    cout << "Hello, " << first_name << 
+        " (age in months: "<< age_in_months <<")\n";
     return 0;   
 }
